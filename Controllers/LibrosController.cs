b2f83b572c5d7a8477a0c6c21264b622dee0399c
David <sergio.valencia@colimasoft.com>
2022-01-28 16:24:31 -0600

    24.- Data Relacionada - Segundo Controlador

---
+++ Controllers/LibrosController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/libros")]
+    public class LibrosController : ControllerBase
+    {
+        readonly ApplicationDbContext contex;
+        public LibrosController(ApplicationDbContext context)
+        {
+            this.contex = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            return await contex.Libros.Include(x => x.Autor).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Libro libro)
+        {
+            var existeAutor = await contex.Autores.AnyAsync(x => x.Id == libro.AutorId);
+            if (!existeAutor)
+                return BadRequest($"No existe el autor de Id:{ libro.AutorId }");
+
+            contex.Libros.Add(libro);
+            await contex.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
 
 namespace WebAPIAutores
 {
@@ -14,7 +15,7 @@ namespace WebAPIAutores
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             //services.AddEndpointsApiExplorer();
             services.AddDbContext<ApplicationDbContext>(options =>
