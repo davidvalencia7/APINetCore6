9b23cda5f8f657460ace3476c54c77812967fe0f
David <sergio.valencia@colimasoft.com>
2022-01-27 21:22:13 -0600

    20.- Iniciando el Desarrollo del Web API

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPIAutores.Entidades;
+
+namespace WebAPIAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor() { Id = 1, Nombre = "David" },
+                new Autor() { Id = 2, Nombre = "Juan" },
+                new Autor() { Id = 3, Nombre = "Andrea" }
+            };
+        }
+    }
+}
