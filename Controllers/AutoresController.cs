965c6dc7547a390d3518ef22a5a06534ba705cbb
David <sergio.valencia@colimasoft.com>
2022-01-29 15:50:16 -0600

    27. y 28.- Reglas de Ruteo y Variables de Ruta

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -14,10 +14,40 @@ namespace WebAPIAutores.Controllers
             this.context = context;
         }
 
-        [HttpGet]
+        [HttpGet]// api/autores
+        [HttpGet("listado")]// api/autores/listado
+        [HttpGet("/listado")]// listado
         public async Task<ActionResult<List<Autor>>> Get()
         {
             return await context.Autores.Include(x => x.Libros).ToListAsync();
+        }
+
+
+        [HttpGet("primero")]
+        public async Task<ActionResult<Autor>> PrimerAutor()
+        {
+            return await context.Autores.FirstOrDefaultAsync();
+        }
+
+        //[HttpGet("{id:int}/{param?}")]
+        [HttpGet("{id:int}/{param=persona}")]
+        public async Task<ActionResult<Autor>> Get(int id, string param)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null)
+                return NotFound();
+            
+            return Ok(autor);
+        }
+
+        [HttpGet("{nombre}")]
+        public async Task<ActionResult<Autor>> Get(string nombre)
+        {
+            var autor = await context.Autores.FirstOrDefaultAsync(x => x.Nombre.Contains(nombre));
+            if (autor == null)
+                return NotFound();
+
+            return Ok(autor);
         }
 
         [HttpPost]
