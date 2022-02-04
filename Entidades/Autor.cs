500011a09fe2f7a3e4867cd015b534d1a8c19774
David <sergio.valencia@colimasoft.com>
2022-02-04 13:39:38 -0600

    33. - Validaciones por Defecto

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,9 +1,23 @@
-﻿namespace WebAPIAutores.Entidades
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPIAutores.Entidades
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es Requerido")]
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} caracteres")]  
         public string Nombre { get; set; }
+        [Range(18,120)]
+        [NotMapped]
+        public int Edad { get; set; }
+        [CreditCard]
+        [NotMapped]
+        public string TarjetaCredito { get; set; }
+        [Url]
+        [NotMapped]
+        public string URL { get; set; }
         public List<Libro> Libros { get; set; }
     }
 }
