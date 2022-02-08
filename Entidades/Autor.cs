3e816306009b1c43f0c35388160f21351c73efbc
David <sergio.valencia@colimasoft.com>
2022-02-08 12:11:54 -0600

    34.- Validaciones Personalizadas por Atributo

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebAPIAutores.Validaciones;
 
 namespace WebAPIAutores.Entidades
 {
@@ -7,7 +8,8 @@ namespace WebAPIAutores.Entidades
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo {0} es Requerido")]
-        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} caracteres")]  
+        [StringLength(maximumLength: 5, ErrorMessage = "El campo {0} no debe de tener más de {1} caracteres")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         [Range(18,120)]
         [NotMapped]
